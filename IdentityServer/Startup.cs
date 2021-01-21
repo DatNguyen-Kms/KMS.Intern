da00bf442deee40e6117e2977b72be11bfa4db71
Dat Nguyen <thanhdat2751999@gmail.com>
2021-01-21 14:09:20 +0700

    Add project files.

---
+++ IdentityServer/Startup.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace IdentityServer
+{
+    public class Startup
+    {
+        public IWebHostEnvironment Environment { get; }
+
+        public Startup(IWebHostEnvironment environment)
+        {
+            Environment = environment;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            var builder = services.AddIdentityServer()
+                            .AddDeveloperSigningCredential()        //This is for dev only scenarios when you don’t have a certificate to use.
+                            .AddInMemoryApiScopes(Config.ApiScopes)
+                            .AddInMemoryClients(Config.Clients);
+
+            builder.AddDeveloperSigningCredential();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            if (Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseIdentityServer();
+        }
+    }
+}
